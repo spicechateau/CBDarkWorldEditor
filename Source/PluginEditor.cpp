896033b859df50818293bc461ebc7394bdde0a6a
Tim Leete <tz@spicechateau.com>
2021-03-11 23:50:58 -0600

    Basic GUI

---
+++ Source/PluginEditor.cpp
@@ -0,0 +1,174 @@
+/*
+  ==============================================================================
+
+    This file contains the basic framework code for a JUCE plugin editor.
+
+  ==============================================================================
+*/
+
+#include "PluginProcessor.h"
+#include "PluginEditor.h"
+
+//==============================================================================
+CBDarkWorldEditorAudioProcessorEditor::CBDarkWorldEditorAudioProcessorEditor (CBDarkWorldEditorAudioProcessor& p)
+    : AudioProcessorEditor (&p), audioProcessor (p)
+{
+    setSize (300, 420);
+    
+    decaySlider.addListener(this);
+    decaySlider.setSliderStyle (juce::Slider::RotaryHorizontalVerticalDrag);
+    decaySlider.setBounds (1, 60, 100, 100);
+    decaySlider.setRange (1, 127, 1);
+    decaySlider.setColour(1, juce::Colours::white);
+    decaySlider.setTextBoxStyle (juce::Slider::TextBoxBelow, false, 30, 20);
+    addAndMakeVisible (decaySlider);
+    
+    addAndMakeVisible (decayLabel);
+    decayLabel.setText ("Decay", juce::dontSendNotification);
+    decayLabel.setBounds (1, 45, 100, 20);
+    decayLabel.setJustificationType (juce::Justification::centred);
+    
+    mixSlider.addListener(this);
+    mixSlider.setSliderStyle (juce::Slider::RotaryHorizontalVerticalDrag);
+    mixSlider.setBounds (decaySlider.getRight(), 60, 100, 100);
+    mixSlider.setRange (1, 127, 1);
+    mixSlider.setColour(1, juce::Colours::white);
+    mixSlider.setTextBoxStyle (juce::Slider::TextBoxBelow, false, 30, 20);
+    addAndMakeVisible(mixSlider);
+    
+    addAndMakeVisible (mixLabel);
+    mixLabel.setText ("Mix", juce::dontSendNotification);
+    mixLabel.setBounds (decayLabel.getRight(), 45, 100, 20);
+    mixLabel.setJustificationType (juce::Justification::centred);
+    
+    dwellSlider.addListener(this);
+    dwellSlider.setSliderStyle (juce::Slider::RotaryHorizontalVerticalDrag);
+    dwellSlider.setBounds (mixSlider.getRight(), 60, 100, 100);
+    dwellSlider.setRange (1, 127, 1);
+    dwellSlider.setColour(1, juce::Colours::white);
+    dwellSlider.setTextBoxStyle (juce::Slider::TextBoxBelow, false, 30, 20);
+    addAndMakeVisible(dwellSlider);
+    
+    addAndMakeVisible (dwellLabel);
+    dwellLabel.setText ("Dwell", juce::dontSendNotification);
+    dwellLabel.setBounds (mixLabel.getRight(), 45, 100, 20);
+    dwellLabel.setJustificationType (juce::Justification::centred);
+    
+    modifySlider.addListener(this);
+    modifySlider.setSliderStyle (juce::Slider::RotaryHorizontalVerticalDrag);
+    modifySlider.setBounds (1, 185, 100, 100);
+    modifySlider.setRange (1, 127, 1);
+    modifySlider.setColour(1, juce::Colours::white);
+    modifySlider.setTextBoxStyle (juce::Slider::TextBoxBelow, false, 30, 20);
+    addAndMakeVisible(modifySlider);
+    
+    addAndMakeVisible (modifyLabel);
+    modifyLabel.setText ("Modify", juce::dontSendNotification);
+    modifyLabel.setBounds (1, 170, 100, 20);
+    modifyLabel.setJustificationType (juce::Justification::centred);
+    
+    toneSlider.addListener(this);
+    toneSlider.setSliderStyle (juce::Slider::RotaryHorizontalVerticalDrag);
+    toneSlider.setBounds (modifySlider.getRight(), 185, 100, 100);
+    toneSlider.setRange (1, 127, 1);
+    toneSlider.setColour(1, juce::Colours::white);
+    toneSlider.setTextBoxStyle (juce::Slider::TextBoxBelow, false, 30, 20);
+    addAndMakeVisible(toneSlider);
+    
+    addAndMakeVisible (toneLabel);
+    toneLabel.setText ("Tone", juce::dontSendNotification);
+    toneLabel.setBounds (modifyLabel.getRight(), 170, 100, 20);
+    toneLabel.setJustificationType (juce::Justification::centred);
+    
+    preDelaySlider.addListener(this);
+    preDelaySlider.setSliderStyle (juce::Slider::RotaryHorizontalVerticalDrag);
+    preDelaySlider.setBounds (toneSlider.getRight(), 185, 100, 100);
+    preDelaySlider.setRange (1, 127, 1);
+    preDelaySlider.setColour(1, juce::Colours::white);
+    preDelaySlider.setTextBoxStyle (juce::Slider::TextBoxBelow, false, 30, 20);
+    addAndMakeVisible(preDelaySlider);
+    
+    addAndMakeVisible (preDelayLabel);
+    preDelayLabel.setText ("Pre Delay", juce::dontSendNotification);
+    preDelayLabel.setBounds (toneLabel.getRight(), 170, 100, 20);
+    preDelayLabel.setJustificationType (juce::Justification::centred);
+    
+    addAndMakeVisible (darkTypeBox);
+    darkTypeBox.setBounds (10, 330, 80, 20);
+    darkTypeBox.addItem ("Mod", 1);
+    darkTypeBox.addItem ("Shimmer", 2);
+    darkTypeBox.addItem ("Black", 3);
+    darkTypeBox.addListener (this);
+    
+    addAndMakeVisible (darkTypeLabel);
+    darkTypeLabel.setText ("Dark Type", juce::dontSendNotification);
+    darkTypeLabel.setBounds (1, 305, 100, 20);
+    darkTypeLabel.setJustificationType (juce::Justification::centred);
+    
+    addAndMakeVisible (effectOrderBox);
+    effectOrderBox.setBounds (darkTypeBox.getRight() + 20, 330, 80, 20);
+    effectOrderBox.addItem ("Parallel", 1);
+    effectOrderBox.addItem ("D > W", 2);
+    effectOrderBox.addItem ("W > D", 3);
+    effectOrderBox.addListener (this);
+    
+    addAndMakeVisible (effectOrderLabel);
+    effectOrderLabel.setText ("Effect Order", juce::dontSendNotification);
+    effectOrderLabel.setBounds (darkTypeLabel.getRight(), 305, 100, 20);
+    effectOrderLabel.setJustificationType (juce::Justification::centred);
+    
+    addAndMakeVisible (worldTypeBox);
+    worldTypeBox.setBounds (effectOrderBox.getRight() + 20, 330, 80, 20);
+    worldTypeBox.addItem ("Hall", 1);
+    worldTypeBox.addItem ("Plate", 2);
+    worldTypeBox.addItem ("Spring", 3);
+    worldTypeBox.addListener (this);
+    
+    addAndMakeVisible (worldTypeLabel);
+    worldTypeLabel.setText ("World Type", juce::dontSendNotification);
+    worldTypeLabel.setBounds (effectOrderLabel.getRight(), 305, 100, 20);
+    worldTypeLabel.setJustificationType (juce::Justification::centred);
+    
+    addAndMakeVisible (darkOnButton);
+    darkOnButton.setBounds (40, 370, 30, 30);
+    
+    addAndMakeVisible (worldOnButton);
+    worldOnButton.setBounds (240, 370, 30, 30);
+    
+    addAndMakeVisible (darkWorldOnLabel);
+    darkWorldOnLabel.setText ("Dark ON / World ON", juce::dontSendNotification);
+    darkWorldOnLabel.setBounds (50, 365, 200, 40);
+    darkWorldOnLabel.setFont (16.f);
+    darkWorldOnLabel.setJustificationType (juce::Justification::centred);
+}
+
+CBDarkWorldEditorAudioProcessorEditor::~CBDarkWorldEditorAudioProcessorEditor()
+{
+}
+
+//==============================================================================
+void CBDarkWorldEditorAudioProcessorEditor::paint (juce::Graphics& g)
+{
+    // (Our component is opaque, so we must completely fill the background with a solid colour)
+    g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
+
+    g.setColour (juce::Colours::black);
+    g.setFont (30.0f);
+    g.drawFittedText ("Dark World Editor", 50, 10, 200, 25, juce::Justification::centred, 1);
+}
+
+void CBDarkWorldEditorAudioProcessorEditor::resized()
+{
+    // This is generally where you'll want to lay out the positions of any
+    // subcomponents in your editor..
+}
+
+void CBDarkWorldEditorAudioProcessorEditor::sliderValueChanged(juce::Slider *slider)
+{
+//    if (slider == &decaySlider)
+}
+
+void CBDarkWorldEditorAudioProcessorEditor::comboBoxChanged(juce::ComboBox *comboBox)
+{
+    
+}
---
+++ Source/PluginEditor.h
@@ -0,0 +1,58 @@
+/*
+  ==============================================================================
+
+    This file contains the basic framework code for a JUCE plugin editor.
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include <JuceHeader.h>
+#include "PluginProcessor.h"
+
+//==============================================================================
+/**
+*/
+class CBDarkWorldEditorAudioProcessorEditor  : public juce::AudioProcessorEditor, public juce::Slider::Listener, public juce::ComboBox::Listener
+{
+public:
+    CBDarkWorldEditorAudioProcessorEditor (CBDarkWorldEditorAudioProcessor&);
+    ~CBDarkWorldEditorAudioProcessorEditor() override;
+
+    //==============================================================================
+    void paint (juce::Graphics&) override;
+    void resized() override;
+    void sliderValueChanged (juce::Slider * slider) override;
+    void comboBoxChanged (juce::ComboBox * comboBox) override;
+
+private:
+    CBDarkWorldEditorAudioProcessor& audioProcessor;
+    
+    juce::Slider decaySlider;
+    juce::Slider mixSlider;
+    juce::Slider dwellSlider;
+    juce::Slider modifySlider;
+    juce::Slider toneSlider;
+    juce::Slider preDelaySlider;
+    
+    juce::ComboBox darkTypeBox;
+    juce::ComboBox effectOrderBox;
+    juce::ComboBox worldTypeBox;
+    
+    juce::Label decayLabel;
+    juce::Label mixLabel;
+    juce::Label dwellLabel;
+    juce::Label modifyLabel;
+    juce::Label toneLabel;
+    juce::Label preDelayLabel;
+    juce::Label darkTypeLabel;
+    juce::Label effectOrderLabel;
+    juce::Label worldTypeLabel;
+    juce::Label darkWorldOnLabel;
+    
+    juce::ToggleButton darkOnButton;
+    juce::ToggleButton worldOnButton;
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (CBDarkWorldEditorAudioProcessorEditor)
+};
